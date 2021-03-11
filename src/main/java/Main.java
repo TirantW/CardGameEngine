6c04eed4dc4ca3738986470094abe4b762c85c69
maikenfp <maikenfridenp@hotmail.com>
2021-03-11 10:30:13 +0100

    kortspill

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int NCARDS = 26;
+        CardDeck cardDeck = new CardDeck();
+        Cards kort = new Cards();
+        Rules rules = new Rules();
+        Hand handA = new Hand ();
+        Hand handB = new Hand ();
+        for (int i = 0; i < NCARDS; i++) {
+            handA.takeCard(cardDeck.draw());
+            handB.takeCard(cardDeck.draw());
+        }
     }
 }
