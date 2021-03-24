0c90e113a70eaa91eb8c6d922786ed618ec95e85
maikenfp <maikenfridenp@hotmail.com>
2021-03-24 11:25:29 +0100

    laget shuffle metoden

--- src/main/java/CardDeck.java
+++ src/main/java/CardDeck.java
@@ -1,40 +1,39 @@
 package main.java;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class CardDeck {
     private ArrayList<Cards> cards;
 
-    public CardDeck(boolean isFull){
+    public CardDeck() {
         cards = new ArrayList<>();
-        if(isFull){
-            for (Cards.Suit suit: Cards.Suit.values()){
-                for (Cards.Number number: Cards.Number.values()){
-                    cards.add(new Cards(suit, number));
-                }
+        for (Cards.Suit suit : Cards.Suit.values()) {
+            for (Cards.Number number : Cards.Number.values()) {
+                cards.add(new Cards(suit, number));
             }
         }
     }
 
-    public void resetDeck(){
+    public void resetDeck() {
         //sletter gamle kort objekter og resetter dekket til 52 kort.
     }
 
-    public void shuffleDeck(){
-        //resorterer kortstokken
+    public void shuffleDeck() {
+        Collections.shuffle(cards);
     }
 
-    public void cardsRemaningInDeck(){
+    public void cardsRemaningInDeck() {
 
     }
 
-    public Cards drawFromDeck(){
+    public Cards drawFromDeck() {
         //vil trekke kort utifra spillregler
         return new Cards();
     }
 
-    public String toString(){
-        for (int i = 0; i < cards.size(); i++){
+    public String toString() {
+        for (int i = 0; i < cards.size(); i++) {
             System.out.println(cards.get(i));
         }
         return "";
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        CardDeck cardDeck = new CardDeck(true);
+        CardDeck cardDeck = new CardDeck();
+        System.out.println(cardDeck.toString());
+
+        cardDeck.shuffleDeck();
         System.out.println(cardDeck.toString());
 
     }
