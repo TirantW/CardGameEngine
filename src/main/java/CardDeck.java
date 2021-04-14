fbb2b0e947519438eda2974cfa8697d02ae3a123
Harani1104 <69955269+Harani1104@users.noreply.github.com>
2021-04-14 10:37:15 +0200

    Update CardDeck.java

--- src/main/java/CardDeck.java
+++ src/main/java/CardDeck.java
@@ -19,18 +19,11 @@ public class CardDeck {
         //sletter gamle kort objekter og resetter dekket til 52 kort.
     }
     
-     //Endret av Harani (er ikke sikker om det fungerer...)
-    public void shuffleDeck(int n) {
-        int i;
-        int k;
-        int j; 
-        for (k = 0; k < n; k++){
-            i = (int) (cards * Math.random());
-            j = (int) (cards * Math.random());
-        }
+   
+    public void shuffleDeck() {
         Collections.shuffle(cards);
     }
-    //....
+  
 
     public void cardsRemaningInDeck() {
 
