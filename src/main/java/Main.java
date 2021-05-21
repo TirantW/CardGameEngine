d23ad68c59b5bdc2c7f12135182102819d8ee99e
Thomas Waaler Hansen <thomawha@hiof.no>
2021-05-21 14:56:06 +0200

    javadoc changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,7 +27,7 @@ public class Main {
             maiken.drawCard(cardDeck);
 
 
-
+        System.out.println(cardDeck.getCards());
         System.out.println(cardDeck.cardsRemainingInDeck());
         System.out.println(tableDeck.cardsRemainingInDeck());
 
